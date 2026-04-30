ef28d3bf947d2250e46d4d70e38d1ee761b92721
Marlyatou Diallo <marlydjedje5@gmail.com>
2026-04-30 10:13:30 +0200

    Lab 2 : composants statiques LigneBus et ListeLignes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
 import './App.css';
 import Header from './Header';
+import ListeLignes from './ListeLignes';
 import Footer from './Footer';
 
-function Statistique1() {
-  return (
-    <div className="statistique">
-      <span className="statistique-chiffre">10</span>
-      <span className="statistique-libelle">Lignes</span>
-    </div>
-  );
-}
-
-function Statistique2() {
-  return (
-    <div className="statistique">
-      <span className="statistique-chiffre">150</span>
-      <span className="statistique-libelle">Arrêts</span>
-    </div>
-  );
-}
-
-function Statistique3() {
-  return (
-    <div className="statistique">
-      <span className="statistique-chiffre">45</span>
-      <span className="statistique-libelle">Bus actifs</span>
-    </div>
-  );
-}
-
 function App() {
+  const lignes = [
+    { id: 1, numero: "1", depart: "Parcelles Assainies", arrivee: "Plateau", arrets: 14 },
+    { id: 2, numero: "7", depart: "Guediawaye", arrivee: "Place Obe", arrets: 18 },
+    { id: 3, numero: "15", depart: "Pikine", arrivee: "Medina", arrets: 12 },
+    { id: 4, numero: "23", depart: "Ouakam", arrivee: "Grand Dakar", arrets: 10 },
+    { id: 5, numero: "8", depart: "Almadies", arrivee: "Colobane", arrets: 16 },
+    { id: 6, numero: "12", depart: "Yoff", arrivee: "Sandaga", arrets: 11 },
+  ];
+
   return (
     <div className="App">
       <Header />
       <main className="contenu">
-        <p>Bienvenue ! Cette application vous aide à trouver votre ligne de bus à Dakar.</p>
-        <div className="statistiques-container">
-          <Statistique1 />
-          <Statistique2 />
-          <Statistique3 />
-        </div>
+        <ListeLignes lignes={lignes} />
       </main>
       <Footer />
     </div>
