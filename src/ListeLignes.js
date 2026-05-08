67e39d1bfe28a50c6cd5cc62d03a4e474f48d7d0
Marlyatou Diallo <marlydjedje5@gmail.com>
2026-05-08 19:13:18 +0200

    lab 3 : etat, recherche et selection de lignes

--- src/ListeLignes.js
+++ src/ListeLignes.js
@@ -15,7 +15,6 @@ function ListeLignes({ lignes }) {
           depart={ligne.depart}
           arrivee={ligne.arrivee}
           arrets={ligne.arrets}
-          couleur={ligne.couleur}
         />
       ))}
     </div>
